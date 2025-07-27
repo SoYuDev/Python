b33195f6758fc2f6e499132e3e7040b9d9ab3973
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-07-27 13:22:07 +0200

    Pong Game
    2 Players Pong Game using the turtle library

---
+++ 22_PongGame/paddle.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.up()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
